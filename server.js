112aec6e8f23b4145ccb826eab9ef73bfed45b64
mikemiller97 <mike.miller.42797@gmail.com>
2022-08-25 15:01:36 -0500

    attached backend to frontend

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '/client/build')))
+app.use(express.static(path.join(__dirname, "/client/build")))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.set('port', process.env.PORT || "5000")
+app.set("port", process.env.PORT || "5000")
 
-app.get('/', (req, res, next) => {
+app.get("*", (req, res, next) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-app.listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+app.listen(app.get("port"), function(){
+    console.log("Express server listening on port " + app.get("port"));
 });
